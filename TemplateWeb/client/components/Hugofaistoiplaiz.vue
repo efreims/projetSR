f4e20070225aca3492eabb86f21d6d87ca5e9853
backslyde <arnaud.duval@efrei.net>
2022-06-14 10:42:49 +0200

    tiens hugo

---
+++ TemplateWeb/client/components/Hugofaistoiplaiz.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        Page réservé aux membres connectés
+    <div>
+</template>
+
+<script>
+     module.exports = {
+        name : "Accueil",
+        props: {
+            resultlogin:Number,
+        },
+        data(){
+            return{
+                
+            }
+        },
+        methods : {
+        
+    
+        },
+        mounted(){
+            //this.$emit("verif-con")
+        }
+    }
+
+</script>
+
+<style>
+
