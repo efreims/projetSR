ae32aaa5a8b2031be104d25024e5fe8a7d36057a
NoeLefevre <73334531+NoeLefevre@users.noreply.github.com>
2022-06-17 17:08:24 +0200

    Update working with css Home for seb et dylan

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# projetSR
+# Install
+
+`npm install`
+
+# Run
+
+`npm start`
+
+# Env
+`renommer le fichier env en .env`
---
+++ client/components/Accueil.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        Page réservé aux membres connectés
+        <Conversation @submit-password = "submitpassword" @submit-message="submitmessage" :listmessage="listmessage"> </Conversation>
+    </div>
+</template>
+
+<script>
+     module.exports = {
+        name : "Accueil",
+        props: {
+            resultlogin:Number,
+            listmessage:Array
+        },
+        components:{
+            Conversation
+        },
+        data(){
+            return{
+                
+            }
+        },
+        methods : {
+            submitmessage(messagesend){
+                console.log('test')
+                this.$emit('submit-message',messagesend)
+            },
+             submitpassword(password){
+                this.$emit('submit-password',password)
+            }
+    
+        },
+        mounted(){
+            this.$emit("load-data")
+        }
+    }
+
+</script>
+
+<style>
+
