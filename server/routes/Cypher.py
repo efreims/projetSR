fdfd5c1cd81d2aab76bbcde6ce4342e715eff2da
NoeLefevre <73334531+NoeLefevre@users.noreply.github.com>
2022-06-21 09:52:22 +0200

    Script python patch

---
+++ server/routes/Cypher.py
@@ -0,0 +1,16 @@
+import math
+import sys
+import ast
+
+
+def encrypt(key,n,plaintext):
+    cipher = [pow(ord(char), key, n) for char in plaintext]
+    return cipher
+
+input = ast.literal_eval(sys.argv[1]) # dans input on a une chaine de caractères contenant n + clé publique du receveur + texte à transmettre
+
+templist = input['data_sent'].split(".")
+
+output = encrypt(int(templist[0]),int(templist[1]),templist[2])
+
+print(output)
